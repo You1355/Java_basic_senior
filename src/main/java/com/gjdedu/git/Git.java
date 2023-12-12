94c7a9ec5c17ddfea84a94551a68fae8d127ee9d
You <2820098179@qq.com>
2023-12-12 21:57:25 +0800

    合并冲突问题_m

--- src/main/java/com/gjdedu/git/Git.java
+++ src/main/java/com/gjdedu/git/Git.java
@@ -1,13 +1,7 @@
 package com.gjdedu.git;
 
-import java.time.LocalDateTime;
-
 public class Git {
     public static void main(String[] args) {
-        System.out.println("Hello Git~~~");
-        System.out.println("Hello Git222~~~");
-        System.out.println(LocalDateTime.now());
-        System.out.println("hello GIt444~~~~");
-        System.out.println("代码冲突问题！！！");
+        System.out.println("111");
     }
 }
