e9907af4928272005cd3e829bd9a51acdeb08110
You <2820098179@qq.com>
2023-12-12 16:56:42 +0800

    2023/12/12 Spring_Mybatis_0.2

--- src/main/java/com/gjdedu/git/Git.java
+++ src/main/java/com/gjdedu/git/Git.java
@@ -3,5 +3,6 @@ package com.gjdedu.git;
 public class Git {
     public static void main(String[] args) {
         System.out.println("Hello Git~~~");
+        System.out.println("Hello Git222~~~");
     }
 }
