9cd9e52a42af23eb0aa7fab9f7755c9419ca2295
You <2820098179@qq.com>
2023-12-12 16:57:34 +0800

    2023/12/12 Spring_Mybatis_0.3

--- src/main/java/com/gjdedu/git/Git.java
+++ src/main/java/com/gjdedu/git/Git.java
@@ -1,8 +1,11 @@
 package com.gjdedu.git;
 
+import java.time.LocalDateTime;
+
 public class Git {
     public static void main(String[] args) {
         System.out.println("Hello Git~~~");
         System.out.println("Hello Git222~~~");
+        System.out.println(LocalDateTime.now());
     }
 }
