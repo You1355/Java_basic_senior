5d544918c256c77b1ca83442861f2275b016b485
You <2820098179@qq.com>
2023-12-13 18:36:39 +0800

    2023-12-13

--- src/main/java/com/gjdedu/git/Git.java
+++ src/main/java/com/gjdedu/git/Git.java
@@ -4,5 +4,7 @@ package com.gjdedu.git;
 public class Git {
     public static void main(String[] args) {
         System.out.println("111");
+        System.out.println("今天是2023-12-13");
+
     }
 }
