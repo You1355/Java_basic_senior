fe130f6d0e4d9612ebff3c9f68768da28f275a69
You1355 <153072071+You1355@users.noreply.github.com>
2023-12-13 18:54:18 +0800

    v -0.2 Git.java

--- src/main/java/com/gjdedu/git/Git.java
+++ src/main/java/com/gjdedu/git/Git.java
@@ -5,6 +5,7 @@ public class Git {
     public static void main(String[] args) {
         System.out.println("111");
         System.out.println("今天是2023-12-13");
-
+        System.out.println("我现在在gitHub上修改代码");
+        System.out.println("寿司代码");
     }
 }
